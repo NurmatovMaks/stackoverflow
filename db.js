38bd846869962c9b9b7fb6671ca99ca8eb21175a
Maksatbek Nurmatov <maksatbeknurmatov@MacBook-Air-Maksatbek.local>
2021-12-27 18:50:44 +0600

    fixed

--- db.js
+++ db.js
@@ -12,9 +12,10 @@ if (process.env.NODE_ENV === "production") {
     logging: false,
     ssl: true,
     dialectOptions: {
-      require: true,
-      ssl: true,
-      rejectUnauthorized: false,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   });
 } else {
